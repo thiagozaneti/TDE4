274b902a89ddf4c56c0f616684ed71bcb51afc3e
thiagozaneti <dotinha0203@gmail.com>
2025-05-25 17:55:53 -0300

    alteracao da funcao validar entradas vazias

--- validacoes.py
+++ validacoes.py
@@ -5,4 +5,4 @@ def validar_telefone(telefone):
     return  8 <= len(telefone) <= 11
 
 def validar_entrada_vazia(entrada_telefone, entrada_nome, entrada_endereco, entrada_cpf):
-    return len(entrada_cpf) <= 0 and len(entrada_endereco) <= 0 and len(entrada_nome) <= 0 and len(entrada_telefone)
+    return all([entrada_cpf.strip(), entrada_endereco.strip(), entrada_nome.strip(), entrada_telefone.strip()])
